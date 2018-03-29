942bc840dadb5a37e21cc36d0d5eed901680c2f8
thanatos8023 <thanatos8023@gmail.com>
2018-03-29 03:08:05 -0400

    server1 mod

--- refiner.py
+++ refiner.py
@@ -371,10 +371,10 @@ def replaceSubstring(line, newstr, ituple):
 
 if __name__ == '__main__':
 
-    filelist = glob.glob('re_sample.txt')
+    filelist = glob.glob('/home/crawadmin/pdf2txt/*.txt')
 
     for filename in filelist:
         try:
-            normalize(filename, 'refined_data/donga_norm.txt')
+            normalize(filename, 'refined/concile_19th_norm.txt')
         except:
             continue
